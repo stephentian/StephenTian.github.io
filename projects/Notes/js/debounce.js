980b07c3b4e7221e35cca39ad6807473f1db0bf9
Stephen Tian <745752067@qq.com>
2019-08-13 21:16:21 +0800

    debounce & throttle

---
+++ projects/Notes/js/debounce.js
@@ -0,0 +1,20 @@
+/**
+* author: stephentian
+* email: stephentian@foxmail.com
+* day: 2019-8-13
+**/
+
+
+// 防抖
+
+function debouncd(fn, time = 1000) {
+  let timer
+  return function () {
+    const args = arguments
+    const context = this
+    if (timer) clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn(context, args)
+    }, time)
+  }
+}
